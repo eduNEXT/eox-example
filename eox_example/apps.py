b4f59a73cf27f2f62189caa187ed4f966fe879e9
mariagrimaldi <maria.grimaldi@edunext.co>
2021-03-08 12:49:08 -0400

    initial commit

---
+++ eox_example/apps.py
@@ -0,0 +1,49 @@
+"""
+App configuration for eox_example.
+"""
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class EoxExampleConfig(AppConfig):
+    """
+    eduNEXT example plugin configuration.
+    """
+    name = 'eox_example'
+    verbose_name = 'eduNEXT example plugin'
+
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': 'eox_example',
+                'regex': r'^eox_example/',
+                'relative_path': 'urls',
+            },
+            'cms.djangoapp': {
+                'namespace': 'eox_example',
+                'regex': r'^eox_example/',
+                'relative_path': 'urls',
+            }
+        },
+        'settings_config': {
+            'lms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'test': {'relative_path': 'settings.test'},
+                'aws': {'relative_path': 'settings.aws'},
+                'production': {'relative_path': 'settings.production'},
+            },
+            'cms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'test': {'relative_path': 'settings.test'},
+                'aws': {'relative_path': 'settings.aws'},
+                'production': {'relative_path': 'settings.production'},
+            },
+        },
+        "view_context_config": {
+            "lms.djangoapp":  {
+                "course_dashboard": "eox_test.context.get_dashboard_context"
+            }
+        }
+    }
---
+++ eox_example/context.py
@@ -0,0 +1,4 @@
+""" Context file. """
+
+def get_dashboard_context(current_context, *args, **kwargs):
+     return {"custom_value": "Hello from plugin"}
