ddf2a26b6e225ac6c5ac9213cb706104245bba6f
Luis Moreno <morenol@users.noreply.github.com>
2021-03-09 12:07:56 -0400

    Use same order

--- eox_example/actions.py
+++ eox_example/actions.py
@@ -3,8 +3,8 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def post_login(request, user):
+def post_login(user, request):
     """
     Logs the username and the site of this new login.
     """
-    log.info("%s has logged in on site {}", user.username, request.get_host())
+    log.info("%s has logged in on site %s", user.username, request.get_host())
